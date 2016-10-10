080131882134ad068926470ab8cd44d2456c3717
kirby@puppetlabs.com <mk.kirby@gmail.com>
2016-10-10 11:40:27 -0700

    Strings only

--- lib/vmpooler/api/v1.rb
+++ lib/vmpooler/api/v1.rb
@@ -47,7 +47,7 @@ module Vmpooler
       backend.hset('vmpooler__vm__' + vm, 'migration_time', finish)
       logger.log(
         's',
-        '[>] ' + vm + ' migrated from ' + vm_object.summary.runtime.host + ' to ' + host.name + ' in ' + finish + ' seconds')
+        "[>] '" + vm + "' migrated from " + vm_object.summary.runtime.host.name + ' to ' + host.name + ' in ' + finish + ' seconds')
     end
 
     def fetch_single_vm(template)
