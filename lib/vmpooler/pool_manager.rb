2340cce595f8742e932da0c34ae6a3e700c334bb
kirby@puppetlabs.com <mk.kirby@gmail.com>
2016-10-12 12:04:47 -0700

    Set data before attempting to compare

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -466,7 +466,9 @@ module Vmpooler
       vm_object = $vsphere[pool].find_vm(vm) || $vsphere[pool].find_vm_heavy(vm)
       host = $vsphere[pool].find_least_used_compatible_host(vm_object).wait_for_completion
       parent_host = vm_object.summary.runtime.host
-      if host.summary.hardware.uuid == parent_host.summary.hardware.uuid
+      target_host_uuid == host.summary.hardware.uuid
+      source_host_uuid == parent_host.summary.hardware.uuid
+      if source_host_uuid == target_host_uuid
         $logger.log('s', '[ ] [' + pool + "] No migration required for '" + vm + "'")
       else
         start = Time.now
