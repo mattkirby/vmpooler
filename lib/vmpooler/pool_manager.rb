57d400f1dc81e51aa2bf55f4d63cd800b070bd9b
kirby@puppetlabs.com <mk.kirby@gmail.com>
2016-10-21 16:33:28 -0700

    Add find_vsphere_pool_vm method that wrap find_vm and find_vm_heavy to provide a consistent way to lookup VMs without specifying both each time. Clean up log message formatting to be more simple

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -461,18 +461,23 @@ module Vmpooler
       end
     end
 
+    def find_vsphere_pool_vm(pool, vm)
+      $vsphere[pool].find_vm(vm) || $vsphere[pool].find_vm_heavy(vm)
+    end
+
     def _migrate_vm(vm, pool)
       $redis.srem('vmpooler__migrating__' + pool, vm)
-      vm_object = $vsphere[pool].find_vm(vm) || $vsphere[pool].find_vm_heavy(vm)
+      vm_object = find_vsphere_pool_vm(pool, vm)
       host = $vsphere[pool].find_least_used_compatible_host(vm_object)
       parent_host = vm_object.summary.runtime.host
       parent_host_name = parent_host.name
       if host == parent_host
-        $logger.log('s', '[ ] [' + pool + "] No migration required for '" + vm + "'")
+        $logger.log('s', "[ ] [#{pool}] No migration required for '#{vm}'")
       else
         start = Time.now
         $vsphere[pool].migrate_vm_host(vm_object, host)
         finish = '%.2f' % (Time.now - start)
+        $metrics.timing("migrate.#{vm['template']}", finish)
         checkout_to_migration = '%.2f' % (Time.now - Time.parse($redis.hget('vmpooler__vm__' + vm, 'checkout')))
         $redis.hset('vmpooler__vm__' + vm, 'migration_time', finish)
         $redis.hset('vmpooler__vm__' + vm, 'checkout_to_migration', checkout_to_migration)
@@ -586,8 +591,8 @@ module Vmpooler
         if inventory[vm]
           begin
             migrate_vm(vm, pool['name'])
-          rescue
-            $logger.log('s', '[x] [' + pool['name'] + "] '" + vm + "' failed to migrate")
+          rescue => err
+            $logger.log('s', "[x] [#{pool['name']}] '#{vm}' failed to migrate: #{err}")
           end
         end
       end
