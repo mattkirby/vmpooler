e538f01e9de860b52e08545ee1be76cc2e186cbf
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-03-30 16:03:43 -0700

    Simplify next slot

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -796,7 +796,8 @@ module Vmpooler
             end
             $redis.sadd('vmpooler__check__pool', pool['name'])
             $redis.srem('vmpooler__check__pool__pending', pool['name'])
-            next_slot = (slots_available? $redis.smembers('vmpooler__check__pool').count, $config[:config]['task_limit'].to_i) + 1
+            slots_free = slots_available? $redis.smembers('vmpooler__check__pool').count, $config[:config]['task_limit'].to_i
+            next_slot = slots_free + 1
             $logger.log('s', "[ ] [#{pool['name']}] checking pool with slot #{next_slot}")
             $redis.hset("vmpooler__pool__#{pool['name']}", 'slot', next_slot)
             check_pool(pool, next_slot.to_s)
