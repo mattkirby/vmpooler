a893efe2f5edc3814daef55bb1419f846689b6a4
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-09-27 17:12:09 -0700

    correctly reference config

--- lib/vmpooler/providers/vsphere.rb
+++ lib/vmpooler/providers/vsphere.rb
@@ -205,7 +205,7 @@ module Vmpooler
               end
             end
             if vm_target_folder.nil?
-              if $config[:config][:create_folders] == true
+              if $config[:config]['create_folders'] == true
                 dc = si.serviceInstance.find_datacenter(target_datacenter_name)
                 vm_target_folder = dc.vmFolder.traverse(target_folder_path, type=RbVmomi::VIM::Folder, create=true)
               else
