e420fb2175ae72d89502a6511585adef20257d24
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-10-11 14:03:06 -0700

    Increase percentage

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -562,7 +562,7 @@ module Vmpooler
 
       if migration_limit
         max_age = 60
-        percentage_of_hosts = 30
+        percentage_of_hosts = 50
         run_select_hosts(provider, pool_name, provider_name, vm['cluster'], vm['datacenter'], max_age, percentage_of_hosts)
         if migration_count >= migration_limit
           $logger.log('s', "[ ] [#{pool_name}] '#{vm_name}' is running on #{vm['host']}. No migration will be evaluated since the migration_limit has been reached")
