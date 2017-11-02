5f84f3e79f856de061011627272df90568b67882
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-11-02 03:32:15 -0700

    Correct reference to vm object

--- lib/vmpooler/providers/vsphere.rb
+++ lib/vmpooler/providers/vsphere.rb
@@ -903,7 +903,7 @@ module Vmpooler
           redis.sadd('vmpooler__migration', vm_name)
           target_host_name = select_next_host(pool_name, @provider_hosts, vm_hash['architecture'])
           target_host_object = find_host_by_dnsname(connection, target_host_name)
-          finish = migrate_vm_and_record_timing(vm_object, target_host_object, vm_hash['host_name'], target_host_name, redis)
+          finish = migrate_vm_and_record_timing(vm_hash['object'], target_host_object, vm_hash['host_name'], target_host_name, redis)
           #logger.log('s', "Provider_hosts is: #{provider.provider_hosts}")
           logger.log('s', "[>] [#{pool_name}] '#{vm_name}' migrated from #{vm_hash['host_name']} to #{target_host_name} in #{finish} seconds")
           remove_vmpooler_migration_vm(pool_name, vm_name, redis)
