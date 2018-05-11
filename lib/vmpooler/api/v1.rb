a434a74091bb1f6f82d9ac61141fec90039bba1a
kirby@puppetlabs.com <mk.kirby@gmail.com>
2018-05-11 16:16:28 -0700

    Remove vms in excess of configured

--- lib/vmpooler/api/v1.rb
+++ lib/vmpooler/api/v1.rb
@@ -762,9 +762,9 @@ module Vmpooler
         status 204
         result['ok'] = true
       else
-        Vmpooler.config[:pools][pool_index[params[:pool]]]['size'] = params[:size]
+        #Vmpooler.config[:pools][pool_index[params[:pool]]]['size'] = params[:size]
         pools[pool_index[params[:pool]]]['size'] = params[:size]
-        backend.hset('vmpooler_config_poolsize', params[:pool], params[:size])
+        backend.hset('vmpooler__config__poolsize', params[:pool], params[:size])
         status 200
         result['ok'] = true
       end
