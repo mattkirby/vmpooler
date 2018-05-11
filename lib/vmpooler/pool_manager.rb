2395f458adb540a96d528884d3bb6a2bd5658659
kirby@puppetlabs.com <mk.kirby@gmail.com>
2018-05-11 16:46:03 -0700

    Fix references to poolname

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -505,8 +505,8 @@ module Vmpooler
           if options[:pool_size_change]
             ready_size = $redis.scard("vmpooler__ready__#{options[:poolname]}")
             break unless ready_size == initial_ready_size
-            if $redis.hget('vmpooler__config__poolsize', pool['name'])
-              break unless $redis.hget('vmpooler__config__poolsize', pool['name']).to_i == pool['size']
+            if $redis.hget('vmpooler__config__poolsize', options[:poolname])
+              break unless $redis.hget('vmpooler__config__poolsize', options[:poolname]).to_i == pool['size']
             end
           end
         end
