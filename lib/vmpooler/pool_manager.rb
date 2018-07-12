0209ea037e8f60705f24a6d7927e503d072a4333
kirby@puppetlabs.com <mk.kirby@gmail.com>
2018-07-11 19:15:17 -0700

    Check provider name

--- lib/vmpooler/pool_manager.rb
+++ lib/vmpooler/pool_manager.rb
@@ -362,7 +362,7 @@ module Vmpooler
     def purge_vms_and_folders(provider)
       $logger.log('s', 'getting provider name')
       provider_name = provider.name
-      $logger.log('s', 'getting configured folders')
+      $logger.log('s', "getting configured folders for #{provider_name}")
       configured_folders = pool_folders(provider)
       $logger.log('s', 'getting base folders')
       base_folders = get_base_folders(configured_folders)
